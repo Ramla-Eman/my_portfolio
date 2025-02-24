8dc288ab17c3c0b53f2be277267dd45c36a451d1
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-02-24 12:32:15 +0500

    Start creating portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl text-purple-900 w-full flex items-center justify-center h-screen'>Starting Portfolio</h1>
+    </>
+  )
+}
+
+export default App
